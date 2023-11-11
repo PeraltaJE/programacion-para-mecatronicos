68b371b3762ae06122693abb19a413a00673ec2d
PeraltaJE <144559066+PeraltaJE@users.noreply.github.com>
2023-11-11 14:52:00 -0500

    Update main.c

--- loteria/main.c
+++ loteria/main.c
@@ -1,3 +1,3 @@
-Para poder ver la loteria en sus diferentes versiones necesita ver los branchs,
-puede entrar al de su preferencia y darle a compare, asi podra ver
-las 3 versiones de la loteria que hemos realizado.
+Para poder ver la loteria en sus diferentes versiones necesita entrar en los branchs,
+puede entrar al de su preferencia, luego darle clcik en contribute y luego a compare, 
+asi podra ver las 3 versiones de la loteria que hemos realizado.
