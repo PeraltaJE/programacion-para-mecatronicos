90e26cc6679f6f63dcb0bc5c831e65be29911508
PeraltaJE <144559066+PeraltaJE@users.noreply.github.com>
2023-11-11 14:19:29 -0500

    Add files via upload

---
+++ loteria/main.c
@@ -0,0 +1,3 @@
+Para poder ver la loteria en sus diferentes versiones necesita ver los branchs,
+puede entrar al de su preferencia y darle a compare, asi podra ver
+las 3 versiones de la loteria que hemos realizado.
